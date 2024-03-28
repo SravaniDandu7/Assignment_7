8965225c23f73d9585e42b3d921f94c31e502389
SravaniDandu7 <147130492+SravaniDandu7@users.noreply.github.com>
2024-03-28 17:43:59 +0530

    Create Vowel.java

---
+++ Vowel.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class VowelChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine().toLowerCase(); 
+        
+        int vowelCount = 0;
+        boolean vowelPresent = false;
+        
+        
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            
+            
+            if (isVowel(ch)) {
+                vowelPresent = true;
+                vowelCount++;
+            }
+        }
+        
+        // Display the result
+        if (vowelPresent) {
+            System.out.println("Vowels are present in the string.");
+            System.out.println("Total number of vowels: " + vowelCount);
+        } else {
+            System.out.println("No vowels are present in the string.");
+        }
+    }
+    
+    
+    public static boolean isVowel(char ch) {
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
+    }
+}
